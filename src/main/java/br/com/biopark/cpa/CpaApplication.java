bf0f9bf73cf8fc51181480e9b52885b1aafdf3d1
João Pedro Gama <joaocletto@gmail.com>
2023-02-17 21:45:54 -0300

    alterando arquivos

---
+++ src/main/java/br/com/biopark/cpa/CpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.biopark.cpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/biopark/cpa/CpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.biopark.cpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
