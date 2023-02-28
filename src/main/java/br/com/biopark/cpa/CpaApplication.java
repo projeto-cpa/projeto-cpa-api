014d3d0215d059f9627e5f82c8ae0ddd1b6dca04
Lucas de Oliveira Neitzke <luka.pc.pc@gmail.com>
2023-02-27 21:45:00 -0300

    inclusao de entidades
    Entidades incluidas:
    Funcionário
    Cargo

--- src/main/java/br/com/biopark/cpa/CpaApplication.java
+++ src/main/java/br/com/biopark/cpa/CpaApplication.java
@@ -3,10 +3,17 @@ package br.com.biopark.cpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.biopark.cpa.entidades.Funcionario;
+import br.com.biopark.cpa.entidades.cargos.Aluno;
+
 @SpringBootApplication
 public class CpaApplication {
 
 	public static void main(String[] args) {
+		Funcionario funcionario = new Funcionario();
+		Aluno aluno = new Aluno();
+		System.out.println("Aqui:");
+		System.out.println(aluno);
 		SpringApplication.run(CpaApplication.class, args);
 	}
 
