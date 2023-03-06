0b42f507149f882e468ca81356f380ab5c4b9b39
João Pedro Gama <joaocletto@gmail.com>
2023-03-06 20:00:06 -0300

    Desenvolvido endpoint de cadastro de usuario

---
+++ src/main/java/br/com/biopark/cpa/models/Cargo.java
@@ -0,0 +1,36 @@
+package br.com.biopark.cpa.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Cargo")
+public class Cargo {
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "id_cargo")
+    @Getter @Setter
+    private long id;
+
+    @NotNull
+    @Column
+    @Getter @Setter
+    private String nome;
+
+    @Column(name = "created_at")
+    @Getter @Setter
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Getter @Setter
+    private Date updatedAt;
+}
---
+++ src/main/java/br/com/biopark/cpa/models/Usuario.java
@@ -0,0 +1,73 @@
+package br.com.biopark.cpa.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_usuario")
+    @Getter @Setter
+    private long id;
+
+    @NotNull
+    @Column
+    @Getter @Setter
+    private String nome;
+
+    @NotNull
+    @Column 
+    @Getter @Setter
+    private String sobrenome;
+
+    @NotNull
+    @Column
+    @Getter @Setter
+    private String senha;
+
+    @NotNull
+    @Column(name = "data_nascimento")
+    @Getter @Setter
+    private Date dataNascimento;
+
+    @NotNull
+    @JoinColumn(name = "id_cargo")
+    @ManyToOne
+    @Getter @Setter
+    private Cargo cargo;
+
+    @Column(name = "created_at")
+    @Getter @Setter
+    private Date createdAt;
+
+    @Column(name = "updated_at") 
+    @Getter @Setter
+    private Date updatedAt;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String nome, String sobrenome, String senha, Cargo cargo, Date dataNascimento) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.senha = senha;
+        this.setCargo(cargo);
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.dataNascimento = dataNascimento;
+    }
+}
