b60b925a5701b6cdf4de1a6c6fed318a40486ad5
Lucas de Oliveira Neitzke <luka.pc.pc@gmail.com>
2023-03-23 19:47:43 -0300

    integração de testes de persistencia

--- src/main/java/br/com/biopark/cpa/repository/CargoRepository.java
+++ src/main/java/br/com/biopark/cpa/repository/CargoRepository.java
@@ -1,11 +1,14 @@
 package br.com.biopark.cpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.biopark.cpa.models.Cargo;
 
+@Repository
 public interface CargoRepository extends JpaRepository<Cargo, Long>{
     
     public Cargo findById(long id);
+    public Cargo findByNome(String nome);
     
 }
