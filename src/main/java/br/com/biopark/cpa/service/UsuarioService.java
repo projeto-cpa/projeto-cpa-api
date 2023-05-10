9fb7dc3d4317da9a18c582631081f25b87c5323d
Matheus Schuch <102114294+MatheusShu1@users.noreply.github.com>
2023-05-10 15:28:44 -0300

    Revert "Detalhes de usuario"
    This reverts commit 1f20ec4df1cc466cad75318922eadb4927067cf2.

--- src/main/java/br/com/biopark/cpa/service/UsuarioService.java
+++ src/main/java/br/com/biopark/cpa/service/UsuarioService.java
@@ -1,13 +1,12 @@
 package br.com.biopark.cpa.service;
 
-import java.util.Optional;
-import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.biopark.cpa.models.Usuario;
 import br.com.biopark.cpa.repository.UsuarioRepository;
-import jakarta.persistence.Id;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -15,39 +14,33 @@ public class UsuarioService {
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    public Usuario cadastrar(Usuario usuario) throws Exception {
 
-        Usuario usuarioCadastrado = new Usuario();
-
+    public Usuario cadastrar(Usuario usuario) throws Exception{
         try {
-            usuarioCadastrado = usuarioRepository.save(usuario);
+            return usuarioRepository.save(usuario);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            throw new Exception("Erro ao cadastrar usuario " + e.getCause());
         }
+    }
 
-        return usuarioCadastrado;
+    public Usuario buscarUsuario(String login) throws Exception {
+        try {
+            return usuarioRepository.findByEmail(login);
+        } catch (Exception e) {
+            throw new Exception("Erro ao buscar usuário");
+        }
+    }
 
+    public Usuario buscarUsuarioPeloEmail(String email) throws Exception {
+        try {
+            return usuarioRepository.findByEmail(email);
+        } catch (Exception e) {
+            throw new Exception("Erro ao buscar usuário");
+        }
     }
 
     public Iterable<Usuario> listarUsuario() {
         return usuarioRepository.findAll();
     }
 
-    public Usuario pegarUsuario(Long id) {
-        Optional<Usuario> usuario = usuarioRepository.findById(id);
-
-        if (usuario.isPresent())
-            return usuario.get();
-
-        return null;
-    }
-
-    public Usuario atualizar(Long id, String senha) {
-        Usuario usuario = usuarioRepository.findById(id).get();
-        usuario.setSenha(senha);
-        usuarioRepository.save(usuario);
-        return usuario;
-    }
-
-    }
-
+}
