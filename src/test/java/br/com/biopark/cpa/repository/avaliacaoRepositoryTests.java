635886ed0198f8bc04cae63c29fff5c115f82108
Juliano Henrique Timm <julianohenriquetimm@gmail.com>
2023-05-14 09:18:05 -0300

    teste de avaliação funcionando

--- src/test/java/br/com/biopark/cpa/repository/avaliacaoRepositoryTests.java
+++ src/test/java/br/com/biopark/cpa/repository/avaliacaoRepositoryTests.java
@@ -13,10 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import br.com.biopark.cpa.models.Avaliacao;
+import br.com.biopark.cpa.models.Eixo;
 import br.com.biopark.cpa.models.Pergunta;
 import br.com.biopark.cpa.models.Turma;
+import br.com.biopark.cpa.models.enums.TipoPergunta;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -30,32 +33,44 @@ public class avaliacaoRepositoryTests {
 
     @Autowired
     private AvaliacaoRepository avaliacaoRepository;
-    private PerguntaRepository perguntaRepository;
-    private TurmaRepository turmaRepository;
+
+    @Autowired
+    private EixoRepository eixoRepository;
 
     @Test
     public void testePersistenciaAvaliacao() throws Exception {
         // cria, persiste e limpa
 
         //cria os atributos com os valores que o construtor da classe avaliação espera
-        String titulo = "Avaliacao 1";
+        Eixo eixo = new Eixo("Primeiro eixo", "Eixo 1", true);
+        entityManager.persist(eixo);
 
-        List<Long> listaIDPergunta = new ArrayList<>();
-        listaIDPergunta.add(1L);
-        listaIDPergunta.add(2L);
-        listaIDPergunta.add(3L);
+        Pageable pageable = PageRequest.of(0, 5);
+        Eixo eixoPersistido = eixoRepository.findByNome(eixo.getNome(), pageable).getContent().get(0);
 
-        List<Long> ListaIDTurma = new ArrayList<>();
-        ListaIDTurma.add(1l);
-        ListaIDTurma.add(2l);
-        ListaIDTurma.add(3l);
+        Pergunta perguntaUm = new Pergunta("Pergunta 1", TipoPergunta.DESCRITIVA, true, eixoPersistido);
+        entityManager.persist(perguntaUm);
+
+        Pergunta perguntaDois = new Pergunta("Pergunta 2", TipoPergunta.AVALIATIVA, true, eixoPersistido);
+        entityManager.persist(perguntaDois);
+
+        Turma turmaUm = new Turma("Turma 1", "Turma que fez a prova", true);
+        entityManager.persist(turmaUm);
+        
+        Turma turmaDois = new Turma("Turma 2", "Turma que fez a prova denovo", true);
+        entityManager.persist(turmaDois);
+
+        List<Pergunta> listaPerguntas = new ArrayList<>();
+        listaPerguntas.add(perguntaUm);
+        listaPerguntas.add(perguntaDois);
+
+        List<Turma> listaTurmas = new ArrayList<>();
+        listaTurmas.add(turmaUm);
+        listaTurmas.add(turmaDois);
 
         Date data = new Date();
 
-        List<Pergunta> listaPerguntas = perguntaRepository.findAllByIdIn(listaIDPergunta);
-        List<Turma> listaTurmas = turmaRepository.findAllByIdIn(ListaIDTurma);
-
-        Avaliacao avaliacao = new Avaliacao(titulo, listaPerguntas, listaTurmas, data);
+        Avaliacao avaliacao = new Avaliacao("Avaliação teste", listaPerguntas, listaTurmas, data);
         entityManager.persist(avaliacao);
 
         Avaliacao persistidoAvaliacao = avaliacaoRepository.findByTitulo(avaliacao.getTitulo());
@@ -67,11 +82,8 @@ public class avaliacaoRepositoryTests {
         Assert.assertNotNull(persistidoAvaliacao);
         Assert.assertEquals(persistidoAvaliacao.getTitulo(), avaliacao.getTitulo());
         Assert.assertEquals(persistidoAvaliacao.getPerguntaList(), avaliacao.getPerguntaList());
-        Assert.assertEquals(persistidoAvaliacao.getRespostaList(), avaliacao.getRespostaList());
         Assert.assertEquals(persistidoAvaliacao.getTurmaList(), avaliacao.getTurmaList());
-        Assert.assertEquals(persistidoAvaliacao.getUsuarioList(), avaliacao.getUsuarioList());
-        Assert.assertEquals(persistidoAvaliacao.getDataCriacao(), avaliacao.getDataCriacao());
-        Assert.assertEquals(persistidoAvaliacao.getDataAtualizacao(), avaliacao.getDataAtualizacao());
+        Assert.assertEquals(persistidoAvaliacao.getDataExpiracao(), avaliacao.getDataExpiracao());
         
     }
 }
