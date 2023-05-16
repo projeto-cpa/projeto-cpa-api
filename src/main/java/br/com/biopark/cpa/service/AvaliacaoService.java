fecd1c39e240a2aad5b55fee38c52b3b8ea960d1
João Pedro Gama <joaocletto@gmail.com>
2023-05-16 19:58:48 -0300

    feature/avaliacao - desenvolvido listagem de avaliacao por Id

--- src/main/java/br/com/biopark/cpa/service/AvaliacaoService.java
+++ src/main/java/br/com/biopark/cpa/service/AvaliacaoService.java
@@ -2,12 +2,13 @@ package br.com.biopark.cpa.service;
 
 import br.com.biopark.cpa.models.Avaliacao;
 import br.com.biopark.cpa.repository.AvaliacaoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AvaliacaoService {
@@ -30,5 +31,16 @@ public class AvaliacaoService {
 
     public Page<Avaliacao> listar(Pageable pageable) {
         return avaliacaoRepository.findAll(pageable);
+    }
+
+    public Avaliacao buscarPorId(long avaliacaoId) {
+
+        Optional<Avaliacao> avaliacao = avaliacaoRepository.findById(avaliacaoId);
+
+        if (avaliacao.isPresent())
+            return avaliacao.get();
+        else
+            throw new EntityNotFoundException("Avaliação não encontrada");
+
     }
 }
