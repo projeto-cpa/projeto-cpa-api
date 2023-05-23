20bc0706e261e866cd74f3ad37a01ee48f9ff5f2
Juliano Henrique Timm <julianohenriquetimm@gmail.com>
2023-05-23 12:40:15 -0300

    primeiras adicoes para imortacao de usuarios por csv

---
+++ src/main/java/br/com/biopark/cpa/controller/UsuarioCSVController.java
@@ -0,0 +1,53 @@
+package br.com.biopark.cpa.controller;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.univocity.parsers.csv.CsvParserSettings;
+import com.univocity.parsers.csv.CsvParser;
+import com.univocity.parsers.common.record.Record;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.biopark.cpa.models.UsuarioCSV;
+import br.com.biopark.cpa.repository.UsuarioCSVRepository;
+import jakarta.transaction.Transactional;
+
+@RestController
+@RequestMapping("/importar")
+@CrossOrigin(origins = { "http://localhost:8080", "http://localhost:3005" })
+@Transactional
+public class UsuarioCSVController {
+
+    @Autowired
+    private UsuarioCSVRepository usuarioCSVRepository;
+
+    @PostMapping(value = "/importarUsuario")
+    public List<UsuarioCSV> importarUsuarioCSV(@RequestParam("file") MultipartFile file) throws Exception {
+        List<UsuarioCSV> usuarioCSV = new ArrayList<>();
+        InputStream inputStream = file.getInputStream();
+        CsvParserSettings csvParserSettings = new CsvParserSettings();
+
+        csvParserSettings.setHeaderExtractionEnabled(true);
+        CsvParser csvParser = new CsvParser(csvParserSettings);
+
+        List<Record> parseAllRecords = csvParser.parseAllRecords(inputStream);
+
+        parseAllRecords.forEach(record -> {
+            UsuarioCSV usuario = new UsuarioCSV();
+            usuario.setNome(record.getString("nome"));
+            usuario.setEmail(record.getString("email"));
+            usuarioCSV.add(usuario);
+        });
+
+        return usuarioCSVRepository.saveAll(usuarioCSV);
+
+    }
+
+}
--- src/test/java/br/com/biopark/cpa/repository/perguntaRepositoryTests.java
+++ src/test/java/br/com/biopark/cpa/repository/perguntaRepositoryTests.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
+import br.com.biopark.cpa.models.Eixo;
 import br.com.biopark.cpa.models.Pergunta;
 import br.com.biopark.cpa.models.enums.TipoPergunta;
 
@@ -30,7 +32,10 @@ public class perguntaRepositoryTests {
     @Test
     public void testePersistenciaPerguntas() throws Exception {
         // cria, persiste e limpa
-        Pergunta perguntaUm = new Pergunta("Como foi seu dia?", TipoPergunta.DESCRITIVA, true);
+        Eixo eixo = new Eixo("Banco de dados", "Banco de dados ADS", true);
+        entityManager.persist(eixo);
+        
+        Pergunta perguntaUm = new Pergunta("Como foi seu dia?", TipoPergunta.DESCRITIVA, true, eixo);
         entityManager.persist(perguntaUm);
         Pageable pageable = PageRequest.of(0, 5);
         Pergunta persistidoPergunta = perguntaRepository.findByTexto(perguntaUm.getTexto(), pageable).getContent()
