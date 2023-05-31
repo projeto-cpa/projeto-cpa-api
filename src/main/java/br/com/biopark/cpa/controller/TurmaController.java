7c6ac2fb2f240499d964c11e79a14d98a23ed0ad
Juliano Henrique Timm <julianohenriquetimm@gmail.com>
2023-05-30 23:48:32 -0300

    Atualizando o back para a tela de turmas

--- src/main/java/br/com/biopark/cpa/controller/TurmaController.java
+++ src/main/java/br/com/biopark/cpa/controller/TurmaController.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +31,6 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/turma")
-@CrossOrigin(origins = { "http://localhost:8080", "http://localhost:3005" })
 @Transactional
 public class TurmaController {
 
