41c101cf5ee8934ac864ed8b903989ff8723c3ed
JorgeAugustoWilchen <120045345+JorgeAugustoWilchen@users.noreply.github.com>
2023-06-29 20:17:48 -0300

    testes funcionando

--- src/test/java/br/com/biopark/cpa/repository/avaliacaoRepositoryTests.java
+++ src/test/java/br/com/biopark/cpa/repository/avaliacaoRepositoryTests.java
@@ -1,98 +1,93 @@
-// package br.com.biopark.cpa.repository;
+ package br.com.biopark.cpa.repository;
 
-// import java.util.Date;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-// import org.junit.Assert;
-// import org.junit.Rule;
-// import org.junit.Test;
-// import org.junit.rules.ErrorCollector;
-// import org.junit.runner.RunWith;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-// import org.springframework.test.context.junit4.SpringRunner;
-// import org.springframework.data.domain.PageRequest;
-// import org.springframework.data.domain.Pageable;
-// import br.com.biopark.cpa.models.Avaliacao;
-// import br.com.biopark.cpa.models.Eixo;
-// import br.com.biopark.cpa.models.Pergunta;
-// import br.com.biopark.cpa.models.Turma;
-// import br.com.biopark.cpa.models.enums.TipoPergunta;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import br.com.biopark.cpa.models.Avaliacao;
+import br.com.biopark.cpa.models.Curso;
+import br.com.biopark.cpa.models.Eixo;
+import br.com.biopark.cpa.models.Pergunta;
+import br.com.biopark.cpa.models.Turma;
+import br.com.biopark.cpa.models.enums.TipoPergunta;
 
-// @RunWith(SpringRunner.class)
-// @DataJpaTest
-// public class avaliacaoRepositoryTests {
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class avaliacaoRepositoryTests {
 
-// @Rule
-// public ErrorCollector error = new ErrorCollector();
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
 
-// @Autowired
-// private TestEntityManager entityManager;
+    @Autowired
+    private TestEntityManager entityManager;
 
-// @Autowired
-// private AvaliacaoRepository avaliacaoRepository;
+    @Autowired
+    private AvaliacaoRepository avaliacaoRepository;
 
-// @Autowired
-// private EixoRepository eixoRepository;
+    @Autowired
+    private EixoRepository eixoRepository;
 
-// @Test
-// public void testePersistenciaAvaliacao() throws Exception {
-// // cria, persiste e limpa
+    @Test
+    public void testePersistenciaAvaliacao() throws Exception {
+        // cria, persiste e limpa
 
-// //cria os atributos com os valores que o construtor da classe avaliação
-// espera
-// Eixo eixo = new Eixo("Primeiro eixo", "Eixo 1", true);
-// entityManager.persist(eixo);
+        //cria os atributos com os valores que o construtor da classe avaliação espera
+        Eixo eixo = new Eixo("Primeiro eixo", "Eixo 1", true);
+        entityManager.persist(eixo);
 
-// Pageable pageable = PageRequest.of(0, 5);
-// Eixo eixoPersistido = eixoRepository.findByNome(eixo.getNome(),
-// pageable).getContent().get(0);
+        Curso curso = new Curso(true, "Eixo 1", "true");
+        entityManager.persist(curso);
 
-// Pergunta perguntaUm = new Pergunta("Pergunta 1", TipoPergunta.DESCRITIVA,
-// true, eixoPersistido);
-// entityManager.persist(perguntaUm);
+        Pageable pageable = PageRequest.of(0, 5);
+        Eixo eixoPersistido = eixoRepository.findByNome(eixo.getNome(), pageable).getContent().get(0);
 
-// Pergunta perguntaDois = new Pergunta("Pergunta 2", TipoPergunta.AVALIATIVA,
-// true, eixoPersistido);
-// entityManager.persist(perguntaDois);
+        Pergunta perguntaUm = new Pergunta("Pergunta 1", TipoPergunta.DESCRITIVA, true, eixoPersistido);
+        entityManager.persist(perguntaUm);
 
-// Turma turmaUm = new Turma("Turma 1", "Turma que fez a prova", true);
-// entityManager.persist(turmaUm);
+        Pergunta perguntaDois = new Pergunta("Pergunta 2", TipoPergunta.AVALIATIVA, true, eixoPersistido);
+        entityManager.persist(perguntaDois);
+
+        Turma turmaUm = new Turma("Turma 1", "Noturno", curso, "turma teste");
+        entityManager.persist(turmaUm);
+
+        Turma turmaDois = new Turma("Turma 2", "Vespertino", curso, "Turma teste 2");
+        entityManager.persist(turmaDois);
+
+        List<Pergunta> listaPerguntas = new ArrayList<>();
+        listaPerguntas.add(perguntaUm);
+        listaPerguntas.add(perguntaDois);
+
+        List<Turma> listaTurmas = new ArrayList<>();
+        listaTurmas.add(turmaUm);
+        listaTurmas.add(turmaDois);
+
+        Date data = new Date();
+
+        Avaliacao avaliacao = new Avaliacao("Avaliação teste", listaPerguntas, listaTurmas, data);
+        entityManager.persist(avaliacao);
+
+        Avaliacao persistidoAvaliacao = avaliacaoRepository.findByTitulo(avaliacao.getTitulo());
         
-// Turma turmaDois = new Turma("Turma 2", "Turma que fez a prova denovo", true);
-// entityManager.persist(turmaDois);
+        entityManager.flush();
+        entityManager.clear();
 
-// List<Pergunta> listaPerguntas = new ArrayList<>();
-// listaPerguntas.add(perguntaUm);
-// listaPerguntas.add(perguntaDois);
-
-// List<Turma> listaTurmas = new ArrayList<>();
-// listaTurmas.add(turmaUm);
-// listaTurmas.add(turmaDois);
-
-// Date data = new Date();
-
-// Avaliacao avaliacao = new Avaliacao("Avaliação teste", listaPerguntas,
-// listaTurmas, data);
-// entityManager.persist(avaliacao);
-
-// Avaliacao persistidoAvaliacao =
-// avaliacaoRepository.findByTitulo(avaliacao.getTitulo());
+        // afirmacoes
+        Assert.assertNotNull(persistidoAvaliacao);
+        Assert.assertEquals(persistidoAvaliacao.getTitulo(), avaliacao.getTitulo());
+        Assert.assertEquals(persistidoAvaliacao.getPerguntaList(), avaliacao.getPerguntaList());
+        Assert.assertEquals(persistidoAvaliacao.getTurmaList(), avaliacao.getTurmaList());
+        Assert.assertEquals(persistidoAvaliacao.getDataExpiracao(), avaliacao.getDataExpiracao());
         
-// entityManager.flush();
-// entityManager.clear();
-
-// // afirmacoes
-// Assert.assertNotNull(persistidoAvaliacao);
-// Assert.assertEquals(persistidoAvaliacao.getTitulo(), avaliacao.getTitulo());
-// Assert.assertEquals(persistidoAvaliacao.getPerguntaList(),
-// avaliacao.getPerguntaList());
-// Assert.assertEquals(persistidoAvaliacao.getTurmaList(),
-// avaliacao.getTurmaList());
-// Assert.assertEquals(persistidoAvaliacao.getDataExpiracao(),
-// avaliacao.getDataExpiracao());
-        
-// }
-// }
+    }
+}
